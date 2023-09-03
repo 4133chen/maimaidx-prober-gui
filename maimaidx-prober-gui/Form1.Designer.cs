cd9adc756fd9090f94ee4f6844ff8c30afcba2dd
4133chen <f20020101@163.com>
2023-09-03 17:28:48 +0800

    更改了窗体问题

--- maimaidx-prober-gui/Form1.Designer.cs
+++ maimaidx-prober-gui/Form1.Designer.cs
@@ -353,7 +353,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "舞萌 DX更新器";
-            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
